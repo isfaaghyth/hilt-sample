bdf6686a0e8ec1bd8656a069c4c05ad10e59515b
isfaaghyth <isfaaghyth@gmail.com>
2020-06-18 02:19:23 +0700

    feat(app:created): database hilt DI module

---
+++ app/src/main/java/app/isfaaghyth/hilt/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package app.isfaaghyth.hilt.di
+
+import android.content.Context
+import androidx.room.Room
+import app.isfaaghyth.hilt.data.NoteDatabase
+import app.isfaaghyth.hilt.data.local.NoteDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(
+        @ApplicationContext context: Context
+    ): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "note.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideLogDao(db: NoteDatabase): NoteDao {
+        return db.noteDao()
+    }
+
+}
